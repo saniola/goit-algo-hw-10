a6ff66438c3eb7cdecb9fd8a4029434bbaf382d4
Alex Kydanov <a.kidanov@gmail.com>
2024-02-07 11:27:17 +0200

    fixes

--- task_01.py
+++ task_01.py
@@ -10,10 +10,10 @@ fruit_juice = pulp.LpVariable("Fruit_Juice", lowBound=0, cat=pulp.LpInteger)
 model += lemonade + fruit_juice, "Total_Product_Quantity"
 
 # Constraints
-model += 2 * lemonade + 2 * fruit_juice <= 100, "Water_Constraint"
-model += lemonade + fruit_juice <= 50, "Sugar_Constraint"
+model += 2 * lemonade + fruit_juice <= 100, "Water_Constraint"
+model += lemonade <= 50, "Sugar_Constraint"
 model += lemonade <= 30, "Lemon_Juice_Constraint"
-model += 2 * fruit_juice + lemonade <= 40, "Fruit_Puree_Constraint"
+model += 2 * fruit_juice <= 40, "Fruit_Puree_Constraint"
 
 # Solving the model
 model.solve()
